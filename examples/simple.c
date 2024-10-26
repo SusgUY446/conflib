0642a54075ceca3009656cae5b6383257d194226
CI <-ci@not-real.com>
2024-10-26 11:02:11 +0200

    Finished Library and added examples

---
+++ examples/simple.c
@@ -0,0 +1,34 @@
+#include <conflib.h>
+#include <stdio.h>
+
+
+
+
+int main() {
+    Config* config = loadConfig("simple.ini", 5);
+
+    if (config == NULL) {
+        fprintf(stderr, "Error Opening Config File\n");
+        return -1;
+    }
+
+    char* db_hostname = getValue(config, "host");
+    char* db_port = getValue(config, "port");
+    char* db_username = getValue(config, "username");
+    char* db_password = getValue(config, "password");
+    char* debug_mode = getValue(config, "debug");
+
+
+
+    printf("Database Host: %s\n", db_hostname);
+    printf("Database Port: %s\n", db_port);
+    printf("Database Username: %s\n", db_username);
+    printf("Database Password: %s\n", db_password);
+    printf("Debug Mode: %s\n", debug_mode);
+
+
+    freeConfig(config);
+
+
+    return 0;
+}
