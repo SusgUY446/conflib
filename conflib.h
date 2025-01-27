1ec14f81fb7e2ede1cca62c845f4dcd40c2cde5a
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2025-01-27 11:07:57 +0100

    Update the copyright/license header in conflib.h

--- conflib.h
+++ conflib.h
@@ -1,8 +1,47 @@
-/*
-Configlib.h 1.1 by SusgUY446 (https://github.com/susguy446)
+//============================================================================================//
+//                                     (C) 2025 SusgUY446                                     //
+//============================================================================================//
+// Conflib - Simple library for loading and saving configurations in C                        //
+//============================================================================================//
+//                                            LICENSE                                         //
+//                                    4-Clause BSD NON-AI License                             //
+//                                      Copyright 2025 SusgUY446                              //
+//                                                                                            //
+// Redistribution and use in source and binary forms, with or without modification,           //
+// are permitted provided that the following conditions are met:                              //
+//                                                                                            //
+// 1. Redistributions of source code must retain the above copyright notice, this list of     //
+// conditions and the following disclaimer.                                                   //
+//                                                                                            //
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list     //
+// of conditions and the following disclaimer                                                 //
+// in the documentation and/or other materials provided with the distribution.                //
+//                                                                                            //
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used  //
+// to endorse or promote products                                                             //
+// derived from this software without specific prior written permission.                      //
+//                                                                                            //
+// 4. The source code and the binary form, and any modifications made to them may not be used //
+// for the purpose of training or improving machine learning algorithms,                      //
+// including but not limited to artificial intelligence, natural language processing, or data //
+// mining. This condition applies to any derivatives,                                         //
+// modifications, or updates based on the Software code. Any usage of the source code or the  //
+// binary form in an AI-training dataset is considered a breach of this License.              //
+//                                                                                            //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY        //
+// EXPRESS OR IMPLIED WARRANTIES,                                                             //
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR   //
+// A PARTICULAR PURPOSE ARE DISCLAIMED.                                                       //
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, //
+// INCIDENTAL, SPECIAL, EXEMPLARY,                                                            //
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   //
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                                                //
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN        //
+// CONTRACT, STRICT LIABILITY,                                                                //
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      //
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                               //
+//============================================================================================//
 
-LICENSE: see LICENSE file
-*/
 
 // Defines
 #define MAX_LINE_LENGTH 256
